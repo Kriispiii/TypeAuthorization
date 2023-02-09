cabcb0d3353812465bab22f46286d1a273dacfae
Некрасов Константин <NekrasovKK@quarta.su>
2023-02-09 11:18:27 +0300

    Initial commit

---
+++ src/main/java/git/kriispiii/typeAuthorization/TypeAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package git.kriispiii.typeAuthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/kriispiii/typeAuthorization/TypeAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package git.kriispiii.typeAuthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
