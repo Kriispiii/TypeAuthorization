9508161150e10edf157e1684f0aa585230e4ac13
Некрасов Константин <NekrasovKK@quarta.su>
2023-02-27 12:18:19 +0300

    Security HTTP Digest authentication - базово работает, но доступ к странице получить не могу

--- src/main/java/git/kriispiii/typeAuthorization/securingweb/WebSecurityConfig.java
+++ src/main/java/git/kriispiii/typeAuthorization/securingweb/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package git.kriispiii.typeAuthorization.securingweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,10 +10,29 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.DigestAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    DigestAuthenticationEntryPoint authenticationEntryPoint() {
+        DigestAuthenticationEntryPoint result = new DigestAuthenticationEntryPoint();
+        result.setRealmName("My App Realm");
+        result.setKey("3028472b-da34-4501-bfd8-a355c42bdf92");
+        return result;
+    }
+
+    DigestAuthenticationFilter digestFilter() {
+        DigestAuthenticationFilter result = new DigestAuthenticationFilter();
+        result.setAuthenticationEntryPoint(authenticationEntryPoint());
+        result.setUserDetailsService(userDetailsService);
+        return result;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,20 +45,17 @@ public class WebSecurityConfig {
                         .loginPage("/login")
                         .permitAll()
                 )
+                .exceptionHandling(e -> e.authenticationEntryPoint(authenticationEntryPoint()))
+                .addFilter(digestFilter())
                 .logout((logout) -> logout.permitAll());
 
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("u")
-                        .password("123")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    public UserDetailsService userDetailsServiceBean() {
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        inMemoryUserDetailsManager.createUser(User.withUsername("u").password("123").roles("USER").build());
+        return inMemoryUserDetailsManager;
     }
 }
